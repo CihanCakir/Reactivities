57941b3bea5c287375149f4b952e33e6f7be3ec2
Cihan Cakir <cihancakir@Cihans-MacBook-Air.local>
2020-04-07 14:44:41 +0300

    Build Development Version

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import { IUser, IUserFormValues } from '../models/user';
 import { IProfile, IPhoto } from '../models/profile';
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 
 axios.interceptors.request.use((config) => {
@@ -43,14 +43,14 @@ axios.interceptors.response.use(undefined, error => {
 const responseBody = (response: AxiosResponse) => response.data;
 
 
-const sleep = (ms: number) => (response: AxiosResponse) =>
-    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+// const sleep = (ms: number) => (response: AxiosResponse) =>
+//     new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
 
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    del: (url: string) => axios.delete(url).then(responseBody),
     postForm: (url: string, file: Blob) => {
         let formData = new FormData();
         formData.append('File', file);
@@ -61,7 +61,7 @@ const requests = {
 }
 
 const Activities = {
-    list: (params: URLSearchParams): Promise<IActivitiesEnvelope> => axios.get(`/activities`, { params: params }).then(sleep(1000)).then(responseBody),
+    list: (params: URLSearchParams): Promise<IActivitiesEnvelope> => axios.get(`/activities`, { params: params }).then(responseBody),
     details: (activityId: string) => requests.get(`/activities/${activityId}`),
     create: (activity: IActivity) => requests.post('/activities', activity),
     update: (activity: IActivity) => requests.put(`/activities/${activity.activityId}`, activity),
